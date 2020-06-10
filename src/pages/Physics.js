1a182e522b82a2f3d9b82137670492af91f13ec4
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-06-10 13:54:45 -0400

    background fix

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -40,14 +40,14 @@ export default function Physics() {
           <h1>Guide to using the physics pages:</h1>
  
           <ul style={{textAlign: "center", background: "#354e01"}}>
-            <li><strong style={{fontSize: "18px"}}>1. CB: Link to Collegeboard's unit outline guide. Includes standards, science practices, and I can statements</strong></li>
+            <li style={{background: "#354e01"}}><strong style={{fontSize: "18px"}}>1. CB: Link to Collegeboard's unit outline guide. Includes standards, science practices, and I can statements</strong></li>
             <li><strong style={{fontSize: "18px"}}>2. NOTES: Link to comprehensive notes guide with example problems throughout</strong></li>
             <li><strong style={{fontSize: "18px"}}>3. NOTES OUTLINE: Link to fill in the blank notes guide with practice problems</strong></li>
             <li><strong style={{fontSize: "18px"}}>4. VOCAB: Link to vocabulary for this unit</strong></li>
             <li><strong style={{fontSize: "18px"}}>5. EXAMPLES: Link to worked examples from the notes tab</strong></li>
             </ul>
         </Card>
-        <ResponsiveGrid className="testertester"></ResponsiveGrid>
+        <ResponsiveGrid style={{background:"#354e01"}} className="testertester"></ResponsiveGrid>
       </div>
     );
   }
