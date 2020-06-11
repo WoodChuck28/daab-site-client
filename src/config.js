a2caec65be57c2094bb1ccf185c091e4ff425405
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-06-11 16:01:49 -0400

    changed bucket

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ const dev = {
     STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
     s3: {
       REGION: "us-east-1",
-      BUCKET: "tickers-app-api-prod-attachmentsbucket-9g0in1ycgl49"
+      BUCKET: "blogs-app-api-prod-attachmentsbucket-i5qe4soom0bf"
     },
     apiGateway: {
       REGION: "us-east-1",
@@ -28,9 +28,9 @@ const dev = {
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_tGLPGSHaT",
-      APP_CLIENT_ID: "285sr5g88gd4kfs6grkpb55399",
-      IDENTITY_POOL_ID: "us-east-1:4087cf02-bd0c-470c-a81e-a47542e45a4d"
+      USER_POOL_ID: "us-east-1_xBjpXsrud",
+      APP_CLIENT_ID: "5o1lb149v16bfougaifk6celg5",
+      IDENTITY_POOL_ID: "us-east-1:d902cfcc-c8c9-4821-9d56-b580435ebdd9"
     }
   };
   
