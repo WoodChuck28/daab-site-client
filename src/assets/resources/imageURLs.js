39f78f3443373ca2d6c41cad10b8ab78de5320b3
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-06-18 20:43:06 -0400

    changed img

---
+++ src/assets/resources/imageURLs.js
@@ -0,0 +1,8 @@
+import image1 from "./img/galaxy2.jpg";
+import image2 from "./img/MyLOGO.png";
+import image3 from "./img/peace-symbol.jpg"
+
+export default function urls() {
+    const urls = {image1, image2, image3}
+    return urls;
+};
