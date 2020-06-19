ca901d25b28f08645ec10d600c5c517b02b7ccef
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-06-19 10:59:33 -0400

    test

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -10,10 +10,11 @@ import BlogsVisual from "../containers/VisualBlogs"
 import BlogsVisual2 from "../containers/VisualBlogs2";
 import BlogsVisual3 from "../containers/VisualBlogs3";
 
-import galaxy_img from "../assets/resources/img/galaxy3.jpg";
-import my_logo from "../assets/resources/img/MyLOGO.png";
-
 import "./styles/Blog.css";
+
+const galaxy_img = require("../assets/resources/img/galaxy3.jpg");
+const my_logo = require("../assets/resources/img/MyLOGO.png");
+
 
 console.log(galaxy_img);
 
